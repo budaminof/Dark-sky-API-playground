7f5f845f7630fbf8bcd81cf48967ba5c91b18636
bud aminof <budaminof@gmail.com>
2017-08-20 11:07:26 -0600

    bug: express.static used the wrong path

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(_root , 'src'));
 
 app.use('/api', users);
 
