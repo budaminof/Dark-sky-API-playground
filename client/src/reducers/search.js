51f8bf045a3e1c822f66c3bf35981e740c7999f5
bud aminof <budaminof@gmail.com>
2017-08-20 15:34:03 -0600

    bug: set error false in search reducer after new search with data comes in set error to false again.

--- client/src/reducers/search.js
+++ client/src/reducers/search.js
@@ -3,7 +3,7 @@ import { NEW_DATA, ERROR } from '../actions/types';
 export default function (state = { error: false }, action) {
     switch (action.type) {
         case NEW_DATA:
-            return {...state, forcast: action.payload };
+            return {...state, forcast: action.payload, error: false };
         case ERROR:
             return {...state, error: action.payload };
 
