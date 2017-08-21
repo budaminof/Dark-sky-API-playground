255016c60044cd20fb0957d150421a81a289637e
Bud Aminof <baminof@sovrn.com>
2017-08-21 14:31:22 -0600

    refactor: white spaces

--- client/test/reducer.spec.js
+++ client/test/reducer.spec.js
@@ -9,17 +9,20 @@ import response from './response';
 describe('----------- Search Reducer ----------------', () => {
 
   it('should return the initial state', () => {
+
     expect(reducer(undefined, {})).to.deep.equal(
       { error: false, forcast: {} }
     )
+
   });
 
   it('should handle ERROR', () => {
+
     expect(reducer([], {
         type: types.ERROR,
         payload: true
-      })
-    ).to.deep.equal({ error: true })
+      })).to.deep.equal({ error: true });
+
   });
 
   it('should handle NEW_DATA', () => {
@@ -27,8 +30,8 @@ describe('----------- Search Reducer ----------------', () => {
     expect(reducer([], {
         type: types.NEW_DATA,
         payload: response
-      })
-    ).to.deep.equal({ forcast: response, error: false })
+      })).to.deep.equal({ forcast: response, error: false });
+
   });
 
 });
