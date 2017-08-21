48761954e470f0f70228d38c7300ae6ab13b3eb9
Bud Aminof <baminof@sovrn.com>
2017-08-21 15:20:31 -0600

    feat-style: added clouds added css classes to display clouds coverage instead of the gradient background color

--- client/src/components/weather/weather.js
+++ client/src/components/weather/weather.js
@@ -4,26 +4,26 @@ import { PropTypes } from 'prop-types';
 export default class Weather extends Component {
 
   render() {
-    let clouds = `linear-gradient( grey, white ${this.props.forecast.data.currently.cloudCover * 100}%)`;
+    // let cloudsBackgroundGradient = `linear-gradient( grey, white ${this.props.forecast.data.currently.cloudCover * 100}%)`;
     let numOfClouds = this.props.forecast.data.currently.cloudCover * 10;
     let cloudCoverage = [];
     for (var i = 0; i < numOfClouds; i++) {
-      let name = `cloud${i}`
+      let name = `cloud${i+1} cloud`
       cloudCoverage.push(<div className={name} key={i}></div>);
     }
 
-
     return (
       <div className='weather'>
-        <div className="forecast col-md-12 col-xs-12"
-          style= {{ backgroundImage: clouds }} >
+
+        <div className="clouds">
+        { cloudCoverage }
+        </div>
+
+        <div className="forecast col-md-12 col-xs-12">
            <h3> { this.props.forecast.data.currently.temperature.toFixed(0) } <span> &deg; </span>
                 { this.props.forecast.data.currently.summary }
            </h3>
            <h4> { this.props.forecast.data.daily.summary } </h4>
-           <div className="clouds">
-             { cloudCoverage }
-           </div>
            <img className="test" src={'/../assets/' + this.props.forecast.data.daily.icon + '.png'} ></img>
         </div>
       </div>
